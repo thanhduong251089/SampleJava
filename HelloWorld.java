84fc069e2078b2fd461488fcc06d034ebc393137
Thanh Duong <thanhduong251089@gmail.com>
2022-01-03 23:06:35 +0700

    Change

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld
 {
     public static void main(String args[]){
         System.out.println("Hello World!");
-        System.out.println("Test Webhook!");
+        System.out.println("Test Webhook1!");
     }
 }
