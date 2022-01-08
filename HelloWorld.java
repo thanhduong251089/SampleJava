321843c7f8ef962e9eb38a2cef92957016dfe60e
Thanh Duong <thanhduong251089@gmail.com>
2022-01-09 00:14:33 +0700

    Add parameter

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ class HelloWorld
     public static void main(String args[]){
         System.out.println("Hello World!");
         System.out.println("Test Webhook1!");
+        System.out.println("Test Parameter!");
     }
 }
